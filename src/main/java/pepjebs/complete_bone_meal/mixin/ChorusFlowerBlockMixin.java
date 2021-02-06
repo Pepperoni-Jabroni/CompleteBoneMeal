74b8334b7ce00fc00bf3f928fdc8e1320cb64d60
Pepperoni-Jabroni <nickrohn18@gmail.com>
2021-02-06 13:52:03 -0800

    Working Bone Meal on Chorus Flowers

---
+++ src/main/java/pepjebs/complete_bone_meal/mixin/ChorusFlowerBlockMixin.java
@@ -0,0 +1,32 @@
+package pepjebs.complete_bone_meal.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChorusFlowerBlock;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.VineBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.Random;
+
+@Mixin(ChorusFlowerBlock.class)
+@Implements(@Interface(iface= Fertilizable.class, prefix = "fertilizable$"))
+public class ChorusFlowerBlockMixin {
+
+    public boolean fertilizable$isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
+    }
+
+    public boolean fertilizable$canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    public void fertilizable$grow(ServerWorld world, Random random, BlockPos pos, BlockState state){
+        ChorusFlowerBlock.generate(world, pos, random, 5);
+    }
+}
